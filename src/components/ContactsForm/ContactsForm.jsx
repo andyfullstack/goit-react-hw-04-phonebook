29a523a3d1fd98eb4842116531a560ef8dd5a100
andyfullstack <andriikoliadenko@gmail.com>
2024-01-13 19:03:56 +0200

    upd

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,49 +1,48 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Form, Input, Button } from './ContactsForm.styled';
 
-class ContactsForm extends Component {
-  state = {
+const ContactsForm = props => {
+  const [state, setState] = useState({
     name: '',
     number: '',
-  };
+  });
 
-  onInputChange = e => {
+  const onInputChange = e => {
     const { name, value } = e.target;
 
-    this.setState({
+    setState(prevState => ({
+      ...prevState,
       [name]: value,
-    });
+    }));
   };
 
-  onFormSubmit = e => {
+  const onFormSubmit = e => {
     e.preventDefault();
-    this.props.onFormSubmit(this.state);
+    props.onFormSubmit(state);
     e.target.reset();
   };
 
-  render() {
-    return (
-      <Form onSubmit={this.onFormSubmit} style={{ margin: 'auto' }}>
-        <label htmlFor="name">Name</label>
-        <Input
-          type="text"
-          name="name"
-          onChange={this.onInputChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          required
-        />
-        <label htmlFor="number">Contact Number</label>
-        <Input
-          type="tel"
-          name="number"
-          onChange={this.onInputChange}
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          required
-        />
-        <Button type="submit">Add contact</Button>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form onSubmit={onFormSubmit} style={{ margin: 'auto' }}>
+      <label htmlFor="name">Name</label>
+      <Input
+        type="text"
+        name="name"
+        onChange={onInputChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        required
+      />
+      <label htmlFor="number">Contact Number</label>
+      <Input
+        type="tel"
+        name="number"
+        onChange={onInputChange}
+        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        required
+      />
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+};
 
 export default ContactsForm;
